05c5497179dea0973fcc1b93a3e1c4c8c559ac56
XGCoder <xggcoder@gmail.com>
2026-05-10 23:02:46 +0800

    分支触发更新测试

--- blog.config.js
+++ blog.config.js
@@ -5,7 +5,6 @@ const BLOG = {
   // Important page_id！！！Duplicate Template from  https://tanghh.notion.site/02ab3b8678004aa69e9e415905ef32a5
   NOTION_PAGE_ID: process.env.NOTION_PAGE_ID || 'd89edc665b5b471287de3566f98227d9',
 
-
   THEME: process.env.NEXT_PUBLIC_THEME || 'heo', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 claude,endspace,example,fukasawa,fuwari,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
 
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
